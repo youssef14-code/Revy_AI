8b74592bcb50577be3ac51c7b69ce9df7cbed64a
Youssef Hazem <yh2767058@gmail.com>
2026-02-04 12:54:47 +0200

    edits

---
+++ retrival/loaders.py
@@ -0,0 +1,12 @@
+import pdfplumber
+
+def extract_text_from_pdf(pdf_path: str) -> str:
+    full_text = []
+
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text = page.extract_text()
+            if text:
+                full_text.append(text)
+
+    return "\n\n".join(full_text)
